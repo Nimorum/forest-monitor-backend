235bd30cc810e952b18293075995ae25a1fc2c52
Nimorum <PitGadGetAngra@gmail.com>
2026-04-09 01:45:48 +0100

    password updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -132,7 +132,9 @@ class AuthController extends Controller
             function ($user, $password) {
                 $user->forceFill([
                     'password' => Hash::make($password)
-                ])->setRememberToken(Str::random(60))->save();
+                ]);
+                $user->setRememberToken(Str::random(60));
+                $user->save();
 
                 event(new PasswordReset($user));
             }
